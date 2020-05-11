45c971f3ecac0cedfe32bb32aa8536ec563a6a8a
Kelvin <keelviinn@gmail.com>
2020-05-11 18:39:27 -0300

    Mail Cancellation and Mail Templates

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+   host: 'smtp.mailtrap.io',
+   port: 2525,
+   secure: false,
+   auth: {
+      user: '1cd4f603535234',
+      pass: '2e297e95c071a4',
+   },
+   default: {
+      from: 'Equipe GoBarber <noreply@gobarber.com>',
+   },
+};
